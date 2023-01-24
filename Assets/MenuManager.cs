7c1b447aed49b71d575b95ae219f5e39f71971fe
SoraxDubbing <sorax5.r23@gmail.com>
2023-01-24 16:20:06 +0100

    main menu not finished but there some things

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -4,18 +4,7 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
     public void QuitGame()
     {
         Application.Quit();
