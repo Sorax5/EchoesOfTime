cb593e228b54f1abf28f865b93b16bfb89227af0
SoraxDubbing <sorax5.r23@gmail.com>
2023-01-24 19:37:29 +0100

    tile future

---
+++ Assets/script/DeadZone.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    private DeadZonesController deadZonesController;
+    
+    private void Awake()
+    {
+        deadZonesController = GetComponentInParent<DeadZonesController>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            deadZonesController.triggerEnter();
+        }
+    }
+}
