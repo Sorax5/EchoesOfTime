c8c1d67232c380477a41902ceda85cd740b0bab9
1ZFly1 <cyrille.pecnik@iut-dijon.u-bourgogne.fr>
2023-01-24 09:11:25 +0100

    change temporality system

---
+++ Assets/script/TemporalityChange.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.script
+{
+    public class TemporalityChange : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject pastTemporality;
+
+        [SerializeField]
+        private GameObject futurTemporality;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.RightShift))
+            {
+                SwitchTemporality();
+            }
+        }
+
+        private void SwitchTemporality()
+        {
+            if (pastTemporality.activeSelf == true)
+            {
+                futurTemporality.SetActive(true);
+                pastTemporality.SetActive(false);
+            }
+            else
+            {
+                pastTemporality.SetActive(true);
+                futurTemporality.SetActive(false);
+            }
+        }
+    }
+}
