6b53f77aa8c84b30264d4f095e02143183d44695
SoraxDubbing <sorax5.r23@gmail.com>
2023-01-24 13:18:22 +0100

    sign controller
    add sign

---
+++ Assets/LifeController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeController : MonoBehaviour
+{
+    
+    [SerializeField]
+    private int life = 3;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
