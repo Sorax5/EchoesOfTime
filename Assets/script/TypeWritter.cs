b2f91534672125a05a96bf11422c34072c7ed4c4
SoraxDubbing <sorax5.r23@gmail.com>
2023-01-24 23:23:14 +0100

    menu

---
+++ Assets/script/TypeWritter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeWritter : MonoBehaviour
+{
+    
+    [SerializeField]
+    private List<String> text;
+    
+    [SerializeField]
+    private Text textUI;
+    
+    [SerializeField]
+    private float delay = 0.1f;
+
+    private int index = 0;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        textUI.text = null;
+    }
+
+    private void FixedUpdate()
+    {
+        
+        
+    }
+
+    IEnumerator ShowLetterByLetter(String text)
+    {
+        for (var i = 0; i < text.Length; i++)
+        {
+            textUI.text += text[i];
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
