e86e7c8d28baa58d1be05195aebd55b7d89291fd
SoraxDubbing <sorax5.r23@gmail.com>
2023-01-25 03:09:50 +0100

    ending scene

---
+++ Assets/ToEnding.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToEnding : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(2);
+        }
+        
+    }
+}
