b52e6a56bab69aecbf631831d559b01721bec0b9
SoraxDubbing <sorax5.r23@gmail.com>
2023-01-25 05:19:31 +0100

    last change

--- Assets/BackToMenu.cs
+++ Assets/BackToMenu.cs
@@ -14,6 +14,6 @@ public class BackToMenu : MonoBehaviour
     IEnumerator returnToMenu()
     {
         yield return new WaitForSeconds(33);
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/ToEnding.cs
+++ Assets/ToEnding.cs
@@ -10,7 +10,7 @@ public class ToEnding : MonoBehaviour
     {
         if (col.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(3);
         }
         
     }
